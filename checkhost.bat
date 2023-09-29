f019309e57b51f2155ca36bd2458d7dee0ae057d
Naymmm <86914784+Naymmmm@users.noreply.github.com>
2023-09-29 16:46:20 +1000

    the big update
    added version display
    added display raw github data
    added exit
    and cool stuff

--- checkhost.bat
+++ checkhost.bat
@@ -1,13 +1,43 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:menu
+cls
+
+echo Hosts File Checker
+echo ---------------------
+
+echo 1. Check the hosts file
+echo 2. Display raw GitHub data
+echo 3. Display Credits and Version
+echo 4. Exit
+
+set /p choice=Enter your choice: 
+
+if "%choice%" == "1" (
+    call :check_hosts
+) else if "%choice%" == "2" (
+    call :display_raw_data
+) else if "%choice%" == "3" (
+    call :display_version_info
+) else if "%choice%" == "4" (
+    exit /b 0
+) else (
+    echo Invalid choice. Please enter 1, 2, or 3.
+    pause
+    goto menu
+)
+
+exit /b 0
+
+:check_hosts
 REM Check if the script is running with administrator privileges
 net session >nul 2>&1
 if %errorlevel% neq 0 (
-    echo Oops! You have to run with administrator in order for this script to work.
-    echo Since the host file is protected, the script needs administrator to check, as it is read only.
+    echo Oops! You need to run this script as an administrator to be able to read the hosts file.
+    echo Please right-click on the script and select "Run as administrator."
     pause
-    exit /b 1
+    goto menu
 )
 
 REM Define the URL of the raw file on GitHub
@@ -17,12 +47,12 @@ REM Define the path to the hosts file
 set "hosts_file=C:\Windows\System32\drivers\etc\hosts"
 
 REM Define a temporary file to store the downloaded content
-set "temp_file=hosts_check.txt"
+set "temp_file=hosts_check_temp.txt"
 
 REM Initialize a variable to count missing lines
 set "missing_count=0"
 
-REM Download the raw file from GitHub
+REM Download the raw file from GitHub silently
 curl -s -o "%temp_file%" "%github_raw_url%"
 
 REM Check for missing lines in the hosts file
@@ -30,7 +60,7 @@ for /f "tokens=*" %%a in (%temp_file%) do (
     set "line_found="
     findstr "%%a" "%hosts_file%" >nul 2>&1
     if errorlevel 1 (
-        echo A line "%%a" is missing in the hosts file! Use CCStopper to sync or add them.
+        echo Line "%%a" is missing in the hosts file.
         set /a "missing_count+=1"
     )
 )
@@ -39,7 +69,26 @@ REM Cleanup: Delete the temporary file
 del "%temp_file%"
 
 REM Display the total count of missing lines
-echo Looks like you are missing %missing_count% lines in your host file! Use CCStopper to sync or add them.
+echo Looks like you are missing %missing_count% values/host lines. Use CCStopper to add them.
 
-REM Pause to see the results (you can remove this line if you want)
+REM Pause to see the results
 pause
+goto menu
+
+:display_raw_data
+REM Define the URL of the raw file on GitHub
+set "github_raw_url=https://raw.githubusercontent.com/eaaasun/CCStopper/data/Hosts.txt"
+
+REM Download and display the raw data from GitHub
+curl -s "%github_raw_url%"
+echo.
+pause
+goto menu
+
+:display_version_info
+REM Display the script version information
+echo You are running Host Checker v1.1
+echo Developed by Naymmm
+echo Feel free to improve by opening a PR in the GitHub Repository!
+pause
+goto menu
